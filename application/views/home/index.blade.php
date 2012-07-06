677e83e9c4cf39ff69a845e2e6df07d206082990
Hirohisa Kawase <hiro.soft@gmail.com>
2012-07-06 10:29:04 +0900

    mid of handling bootstorap on docs

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -1,72 +1,51 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Laravel:Web職人のためのフレームワーク</title>
-	<meta name="viewport" content="width=device-width">
-	{{ HTML::style('laravel/css/style.css') }}
-</head>
-<body>
-	<div class="wrapper">
-        <div style="color:red;">
-            @if (Session::has('warning'))
-                <p>{{ Session::get('warning') }}</p>
-            @endif
-        </div>
-        <div style="color:orangered;">
-            @if (Session::has('notice'))
-                <p>{{ Session::get('notice') }}</p>
-            @endif
-        </div>
-        <div style="color:blue;">
-            @if (Session::has('message'))
-                <p>{{ Session::get('message') }}</p>
-            @endif
-        </div>
-		<header>
-			<h1>Laravel</h1>
-			<h2>Web職人のためのフレームワーク</h2>
+@layout('template')
 
-			<p class="intro-text" style="margin-top: 45px;">
-			</p>
-		</header>
-		<div role="main" class="main">                          
-			<div class="home">
-				<h2>学習領域</h2>
+@section('title')
+Laravel:トップページ
+@endsection
 
-				<p>
-                    今、あなたはデフォルトのホームページを見ています。
-                    このページを生成してのは次のコードです。
-				</p>
+@section('content')
+<div class="hero-unit">
+    <h1>Laravel</h1>
+    <p>Web職人のためのフレームワーク</p>
+</div>
+<div class="row-fluid">
+    <div class="span9">
+        <h2>学習領域</h2>
+        <p>
+            今、あなたはデフォルトのホームページを見ています。
+            このページを生成しているのは次のコードです。
+        </p>
 
-				<pre>{{ path('app') }}routes.php</pre>
+        <pre>{{ path('app') }}routes.php</pre>
 
-				<p>ビューはこちらで見つけることができます：</p>
+        <p>ビューはこちらで見つけることができます：</p>
 
-				<pre>{{ path('app') }}views/home/index.php</pre>
+        <pre>{{ path('app') }}views/home/index.php</pre>
+    </div>
+</div>
+<div class="row-fluid">
+    <div class="span9">
+        <h2>知識で輝く</h2>
+        <p>
+            感謝したくなるほどに驚異的にシンプルな{{ HTML::link('docs', '素晴らしいドキュメント') }}
+            を使用し、学習できます。
+        </p>
+    </div>
+</div>
+<div class="row-fluid">
+    <div class="span9">
+        <h2>美しく創造する</h2>
 
-				<h2>知識で輝く</h2>
-
-				<p>
-                    感謝したくなるほどに驚異的にシンプルな{{ HTML::link('docs', '素晴らしいドキュメント') }}
-					を使用し、学習できます。
-				</p>
-
-				<h2>美しく創造する</h2>
-
-				<p>
-                    さあ、これで準備は整いました。創造の時間がやって来ました！
-                    始めるのにお役に立つリンクを紹介しましょう：
-				</p>
-
-				<ul class="out-links">
-					<li><a href="http://laravel.com">公式Webサイト</a></li>
-					<li><a href="http://forums.laravel.com">Laravelフォーラム</a></li>
-					<li><a href="http://github.com/laravel/laravel">GitHubリポジトリー</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-</body>
-</html>
+        <p>
+            さあ、これで準備は整いました。創造の時間がやって来ました！
+            始めるのにお役に立つリンクを紹介しましょう：
+        </p>
+        <ul class="out-links">
+            <li><a href="http://laravel.com">公式Webサイト</a></li>
+            <li><a href="http://forums.laravel.com">Laravelフォーラム</a></li>
+            <li><a href="http://github.com/laravel/laravel">GitHubリポジトリー</a></li>
+        </ul>
+    </div>
+</div>
+@endsection
--- application/views/template.blade.php
+++ application/views/template.blade.php
@@ -3,23 +3,95 @@
     <head>
         <meta charset="UTF-8">
         <title>@yield('title')</title>
+        {{ HTML::style('css/bootstrap.css') }}
+        {{ HTML::style('css/bootstrap-responsive.css') }}
+        {{ HTML::style('css/docs.css') }}
+
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
     </head>
     <body>
-        <div style="color:red;">
-            @if (Session::has('warning'))
-                <p>{{ Session::get('warning') }}</p>
-            @endif
+        {{-- トップナビ --}}
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container-fluid">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="#">Laravel</a>
+                    <div class="btn-group pull-right">
+                        <a class="btn btn-danger dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="icon-user"></i>
+                            {{ Auth::check() ? Auth::user()->username : 'Guest' }}
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Profile</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Sign Out</a></li>
+                        </ul>
+                    </div>
+                    <div class="nav-collapse">
+                        <ul class="nav">
+                            <li class="active"><a href="{{ URL::base() }}">Home</a></li>
+                            <li><a href="{{ URL::to('docs/home') }}">Documents</a></li>
+                            <li><a href="{{ URL::to('login') }}">Login</a></li>
+                            <li><a href="{{ URL::to('signup') }}">Sing Up</a></li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
         </div>
-        <div style="color:orangered;">
-            @if (Session::has('notice'))
-                <p>{{ Session::get('notice') }}</p>
-            @endif
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span3">
+                    {{-- 左サイドナビ --}}
+                    @yield('sidenave')
+                </div>
+                <div class="span9">
+                    {{-- エラー出力 --}}
+                    @if (Session::has('warning'))
+                    <div class="row-fluid">
+                        <div class="span9 alert alert-error">
+                            <button type="button" class="close" data-dismiss="alert">×</button>
+                            {{ Session::get('warning') }}
+                        </div>
+                    </div>
+                    @endif
+                    @if (Session::has('notice'))
+                    <div class="row-fluid">
+                        <div class="span9 alert">
+                            <button type="button" class="close" data-dismiss="alert">×</button>
+                            {{ Session::get('notice') }}
+                        </div>
+                    </div>
+                    @endif
+                    @if (Session::has('message'))
+                    <div class="row-fluid">
+                        <div class="span9 alert alert-success">
+                            <button type="button" class="close" data-dismiss="alert">×</button>
+                            {{ Session::get('message') }}
+                        </div>
+                    </div>
+                    @endif
+                    {{-- コンテンツ --}}
+                    <div class="row-fluid">
+                        <div class="span9">
+                            @yield('content')
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div style="color:blue;">
-            @if (Session::has('message'))
-                <p>{{ Session::get('message') }}</p>
-            @endif
-        </div>
-　       @yield('content')
-    </body>
+        {{-- Javascript --}}
+        {{ HTML::script('http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js') }}
+        {{ HTML::script('js/bootstrap.min.js') }}
+        {{ HTML::script('laravel/js/prettify.js') }}
+    </div>
+</div>
+</body>
 </html>
