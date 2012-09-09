95070f64dd29efbe68ad446ecc1807daff3c20c6
Hirohisa Kawase <hiro.soft@gmail.com>
2012-09-09 18:55:40 +0900

    Recoverd language files Signed-off-by:Hirohisa Kawase <hiro.soft@gmail.com>

---
+++ application/language/en/auth.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * 認証用、追加メッセージ (Additinal messages for authorization)
+ *
+ */
+
+return array(
+    'success' => 'Login successfully.',
+    'failed' => 'Login failed.',
+    'logoff' => 'Logoff successfully.',
+    'require' => 'Login required.',
+    'created' => 'Create your user accout successfully.',
+);
---
+++ application/language/ja/auth.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * 認証用、追加メッセージ (Additinal messages for authorization)
+ *
+ */
+
+return array(
+    'success' => 'ログインしました。',
+    'failed' => 'ユーザー名とパスワードが一致しません。',
+    'logoff' => 'ログアウトしました。',
+    'require' => 'ログインが必要です。',
+    'created' => '新しいユーザーアカウントを作成しました。',
+);
