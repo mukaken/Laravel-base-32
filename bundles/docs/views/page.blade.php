9c6ab95af00d36ef59d7001633d7a71e34675a54
Hirohisa Kawase <hiro.soft@gmail.com>
2012-09-11 09:07:12 +0900

    Updated view Signed-off-by:Hirohisa Kawase <hiro.soft@gmail.com>

--- bundles/docs/views/page.blade.php
+++ bundles/docs/views/page.blade.php
@@ -10,33 +10,31 @@
 
 @section('footer')
 <hr>
-<p style="font-size:10px;">粗訳完了。翻訳ブラッシュアップ中です。
-    <a href="http://kore1server.com/laravel-tutorial/299-laravel-japanese-document.html">翻訳、ドネーションで、日本語ドキュメントサイト維持にご協力ください。</a>
+<p style="font-size:10px;">
+<a href="http://kore1server.com/laravel-tutorial/299-laravel-japanese-document.html">日本語ドキュメントサイト維持にご協力ください。</a>&nbsp;<a href="http://kore1server.com/laravel-tutorial/323-published-laravel-docs-in-japanese.html">電子書籍版</a>もあります。
 </p>
 <p style="font-size:10px;">
-    {{ HTML::link('http://laravel.kore1server.com', '日本語ドキュメントオリジナルサイト：http://laravel.kore1server.com') }}
+{{ HTML::link('http://laravel.kore1server.com', '日本語ドキュメントオリジナルサイト：http://laravel.kore1server.com') }}
 </p>
 <p style="font-size:10px;">
-    {{ HTML::link('http://kore1server.com/laravel-tutorial.html', '日本語チュートリアル：http://kore1server.com/laravel-tutorial.html') }}
+{{ HTML::link('http://kore1server.com/laravel-tutorial.html', '日本語チュートリアル：http://kore1server.com/laravel-tutorial.html') }}
 </p>
 <p style="font-size:10px;">
-    以下はドキュメントのバックアップサイトです。PHP上でLaravelが動作したPaaSを利用しています。
+以下はドキュメントのバックアップサイトです。PHP上でLaravelが動作したPaaSを利用しています。
 </p>
 <p style="font-size:10px;">
-    {{ HTML::link('http://laravel.phper.jp/docs', 'phper.jp : http://laravel.phper.jp/docs') }}
-</p>
-<p style="font-size:10px;">
-    {{ HTML::link('http://laravel-ja.phpfogapp.com', 'PHP fog : http://laravel-ja.phpfogapp.com') }}
-</p>
-<p style="font-size:10px;">
-    {{ HTML::link('http://laravel-ja.pagodabox.com', 'Pagoda Box : http://laravel-ja.pagodabox.com') }}
-</p>
-<p style="font-size:10px;">
-    {{ HTML::link('http://jadoc-hirokws.rhcloud.com', 'OpenShift : http://jadoc-hirokws.rhcloud.com') }}
+{{ HTML::link('http://laravel.phper.jp/docs', 'phper.jp : http://laravel.phper.jp/docs') }}<br>
+{{ HTML::link('http://laravel-ja.phpfogapp.com', 'PHP fog : http://laravel-ja.phpfogapp.com') }}<br>
+{{ HTML::link('http://laravel-ja.pagodabox.com', 'Pagoda Box : http://laravel-ja.pagodabox.com') }}
 </p>
 <hr>
 <p style="font-size:10px;">
-    {{ HTML::link($ja_link, '日本語') }} /
-    {{ HTML::link($en_link, 'English') }}
+{{ HTML::link($ja_link, '日本語') }} /
+{{ HTML::link($en_link, 'English') }}
+&nbsp;&nbsp;
+{{ HTML::link('/docs/home/0', '最新バージョン(3.2.7)トップ') }} /
+{{ HTML::link('/docs/home/325', '3.2.5トップ') }} /
+{{ HTML::link('/docs/home/324', '3.2.4トップ') }} /
+{{ HTML::link('/docs/home/323', '3.2.3トップ') }}
 </p>
 @endsection
